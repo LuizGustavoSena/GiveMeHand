3d45cb384f91bd49c939e5ac7678f10443a436ff
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-09 17:54:55 -0300

    feat: dynamic decrementMonth

--- src/lib/moment.ts
+++ src/lib/moment.ts
@@ -1,4 +1,4 @@
 import moment from "moment";
 
 moment.locale("pt-br");
-export const formatDateNow = () => moment().subtract(1, 'month').format('MMMM[/]YYYY');
+export const formatDateNow = (decrementMonth: number) => moment().subtract(decrementMonth, 'month').format('MMMM[/]YYYY');
