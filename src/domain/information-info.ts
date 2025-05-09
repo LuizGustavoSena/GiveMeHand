8629282b389b50f4c022f292cd32f372cb0cac09
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-09 17:51:17 -0300

    fix: better print information

--- src/domain/information-info.ts
+++ src/domain/information-info.ts
@@ -2,20 +2,17 @@ import { env } from "@src/lib/env";
 import { InformationInfoContructor } from "./models/information-info-model";
 
 export class InformationInfo {
-    MAIL_HOST = env.MAIL_HOST;
-    MAIL_PORT = env.MAIL_PORT;
-    MAIL_USER = env.MAIL_USER;
-    MAIL_FROM = env.MAIL_FROM;
+    De = env.MAIL_FROM;
 
-    TYPE_EMAIL;
-    MAILS_TO;
-    MAIL_SUBJECT;
-    MAIL_TEXT;
+    Para;
+    Titulo;
+    Mensagem;
+    Tipo;
 
     constructor({ TYPE_EMAIL, MAILS_TO, MAIL_SUBJECT, MAIL_TEXT }: InformationInfoContructor) {
-        this.TYPE_EMAIL = TYPE_EMAIL;
-        this.MAILS_TO = MAILS_TO.join(', ');
-        this.MAIL_SUBJECT = MAIL_SUBJECT;
-        this.MAIL_TEXT = MAIL_TEXT;
+        this.Tipo = TYPE_EMAIL;
+        this.Para = MAILS_TO.join(', ');
+        this.Titulo = MAIL_SUBJECT;
+        this.Mensagem = MAIL_TEXT;
     }
 }
