e145a8a5be3b28057daf0d3c8aa5d51370dffa71
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-19 18:37:59 -0300

    feat: adjust filesToExport to call filesByPath function

--- src/lib/fs.ts
+++ src/lib/fs.ts
@@ -1,12 +1,11 @@
-import { FilenameEnum } from '@src/domain/enums/filename-enum';
 import { filesToExportProps, readFileByPathName } from '@src/domain/models/fs-model';
 import { readdir, readFile, readFileSync } from "fs";
 import { z } from "zod";
 import { formatDateNow } from './moment';
 
-const readFileByPathName = async (pathName: FilenameEnum): Promise<readFileByPathName> => {
+const readFileByPathName = async (pathName: string): Promise<readFileByPathName> => {
     const file = await new Promise<Buffer>((resolve) => {
-        readFile(`./docs/${pathName}.json`, (err, data) => {
+        readFile(`./docs/type-emails/${pathName}.json`, (err, data) => {
             if (err) {
                 console.error(err);;
                 return;
@@ -28,37 +27,43 @@ const readFileByPathName = async (pathName: FilenameEnum): Promise<readFileByPat
         throw new Error(`File ${pathName} is not valid.`);
 
     const data = fileProps.data;
-    const decrementMonth = pathName === FilenameEnum.M ? 0 : 1;
+    const decrementMonth = data.MAIL_SUBJECT.includes('${-1}') ? 1 : 0;
 
     if (data.MAIL_SUBJECT.includes('${Date}'))
-        data.MAIL_SUBJECT = data.MAIL_SUBJECT.replace('${Date}', formatDateNow(decrementMonth));
+        data.MAIL_SUBJECT = data.MAIL_SUBJECT.replace('${Date}', formatDateNow(decrementMonth)).replace('${-1}', '');
 
     if (data.MAIL_TEXT.includes('${Date}'))
-        data.MAIL_TEXT = data.MAIL_TEXT.replace('${Date}', formatDateNow(decrementMonth));
+        data.MAIL_TEXT = data.MAIL_TEXT.replace('${Date}', formatDateNow(decrementMonth)).replace('${-1}', '');
 
     return data;
 };
 
-const filesToExport = async () => {
-    return await new Promise<filesToExportProps[]>((resolve) => {
-        readdir('./docs/attachments', (err, files) => {
-            const nameFiles: filesToExportProps[] = [];
+const filesToExport = async (): Promise<filesToExportProps[]> => {
+    const nameFiles: filesToExportProps[] = [];
 
+    const files = await filesByPath('./docs/attachments');
+
+    files.forEach(file => {
+        const base64content = readFileSync(`docs/attachments/${file}`, { encoding: 'base64' });
+        nameFiles.push({
+            filename: file,
+            content: base64content,
+            encoding: 'base64'
+        });
+    });
+
+    return nameFiles;
+}
+
+export const filesByPath = async (path: string): Promise<string[]> => {
+    return await new Promise((resolve) => {
+        readdir(path, (err, files) => {
             if (err) {
                 console.error('Error reading directory:', err);
                 return;
             }
 
-            files.forEach(file => {
-                const base64content = readFileSync(`docs/attachments/${file}`, { encoding: 'base64' });
-                nameFiles.push({
-                    filename: file,
-                    content: base64content,
-                    encoding: 'base64'
-                });
-            });
-
-            resolve(nameFiles)
+            resolve(files)
         });
     });
 }
