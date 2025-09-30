3ec068391b8cd26edf5dd24fed1032dd0817b4df
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-09-30 20:46:03 -0300

    feat: uppercase format date

--- src/lib/moment.ts
+++ src/lib/moment.ts
@@ -1,4 +1,4 @@
 import moment from "moment";
 
 moment.locale("pt-br");
-export const formatDateNow = (decrementMonth: number) => moment().subtract(decrementMonth, 'month').format('MMMM[/]YYYY');
+export const formatDateNow = (decrementMonth: number) => moment().subtract(decrementMonth, 'month').format('MMMM[/]YYYY').toUpperCase();
