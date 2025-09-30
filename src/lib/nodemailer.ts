7f634ee171bf2e2faf6de90d2a2e88999247e27d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-09-30 20:35:21 -0300

    feat: alter secure mode

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -5,7 +5,7 @@ import { env } from './env';
 const transporter = nodemailer.createTransport({
     host: env.MAIL_HOST,
     port: env.MAIL_PORT,
-    secure: true,
+    secure: false,
     auth: {
         user: env.MAIL_USER,
         pass: env.MAIL_PASS,
